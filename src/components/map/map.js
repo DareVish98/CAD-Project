d98b541c7dcdb6f9a4f92991a3068c7ccc3456cb
andrei8991 <apostolescu.andrei@gmail.com>
2019-12-29 04:58:27 +0000

    Modified map to display Main Page and Listing Map depeneding on provided data

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-var pos = {
+let pos = {
 	lat:59.95,
 	lng:30.33
 };
+let mainPage = true;
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         pos.lat = position.coords.latitude;
@@ -14,36 +13,59 @@ if (navigator.geolocation) {
 	});
 }
 
-class SimpleMap extends Component {
-  static defaultProps = {
-    zoom: 11
-  };
-  
-  renderMarkers(map, maps) {
-    let marker = new maps.Marker({
-      position: pos,
-      map: map,
-      title: 'Hello World!'
-    });
-  }
+let locations = [{pos: {lat: 51, lng: -1.35}, tag: "house1"}, {pos: {lat: 50.97, lng: -1.4}, tag: "house2"}, {pos: {lat: 50.94, lng: -1.33}, tag: "house3"}];
+let routes = [{lat: 50.96, lng: -1.35}, {lat: 50.97, lng: -1.36}, {lat: 50.965, lng: -1.34}, {lat: 50.95, lng: -1.365}];
 
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyDjey9xfdZk_i75f3HqX66p0UnJn-1JqSs' }}
-          defaultCenter={pos}
-		  defaultZoom={this.props.zoom}
-		  options={{fullscreenControl: false}}
-		  onGoogleApiLoaded={({map, maps}) => this.renderMarkers(map, maps)}
-		  yesIWantToUseGoogleMapApiInternals
-        >
-  
-        </GoogleMapReact>
-      </div>
-    );
-  }
+class SimpleMap extends Component {
+    static defaultProps = {
+        zoom: 11
+    };
+
+    renderMap(map, maps) {
+        if (mainPage) {
+            for (let i = 0; i < locations.length; i++) {
+                let marker = new maps.Marker({
+                    position: locations[i].pos,
+                    map: map,
+                    title: locations[i].tag
+                });
+            }
+        }
+        else {
+            let directionsService = new maps.DirectionsService();
+            map.origin = routes[0];
+            for (let i = 0; i < routes.length; i++) {
+                let directionsRenderer = new maps.DirectionsRenderer();
+                directionsRenderer.setMap(map);
+                let request = {
+                    origin: routes[0],
+                    destination: routes[i],
+                    travelMode: "WALKING"
+                };
+                directionsService.route(request, (result, status) => {
+                    if (status === 'OK') {
+                        directionsRenderer.setDirections(result);
+                    }
+                });
+            }
+        }
+    }
+
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{height: '100vh', width: '100%'}}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: 'AIzaSyDjey9xfdZk_i75f3HqX66p0UnJn-1JqSs'}}
+                    defaultCenter={pos}
+                    defaultZoom={this.props.zoom}
+                    options={{fullscreenControl: false}}
+                    onGoogleApiLoaded={({map, maps}) => this.renderMap(map, maps)}
+                    yesIWantToUseGoogleMapApiInternals
+                />
+            </div>
+        );
+    }
 }
 
 export default SimpleMap;
